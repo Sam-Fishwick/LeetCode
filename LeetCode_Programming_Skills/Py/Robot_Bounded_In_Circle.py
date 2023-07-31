02cf739573b1038814cda49cd0457e36f8361717
Sam-Fishwick <sam.fishwick@hotmail.com>
2023-07-31 22:00:08 +0100

    new addition

--- LeetCode_Programming_Skills/Py/Robot_Bounded_In_Circle.py
+++ LeetCode_Programming_Skills/Py/Robot_Bounded_In_Circle.py
@@ -16,3 +16,36 @@
 
 #Return true if and only if there exists a circle in the plane such that the 
 #robot never leaves the circle.
+
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        faces: list[str] = ["N","E","S","W"]
+        facing: int = 400
+        coord: list = [0,0]
+
+        print(f"Facing: {faces[facing%4]}")
+
+        for instruct in instructions:
+            match instruct:
+                case "G": 
+                    match faces[facing%4]:
+                        case "N":
+                            coord[1] += 1
+                            print(f"Go North! coords = {coord}")
+                        case "E":
+                            coord[0] += 1
+                            print(f"Go East! coords = {coord}")
+                        case "S":
+                            coord[1] -= 1
+                            print(f"Go South! coords = {coord}")
+                        case "W":
+                            coord[0] -= 1
+                            print(f"Go West! coords = {coord}")
+                case "L": 
+                    facing -= 1
+                    print(f"Facing: {faces[facing%4]}")
+                case "R": 
+                    facing += 1
+                    print(f"Facing: {faces[facing%4]}")
+                    
+        return coord == [0,0] or faces[facing%4] != "N"
