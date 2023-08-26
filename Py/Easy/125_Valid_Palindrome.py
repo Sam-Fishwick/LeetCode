44260eba45b9535394260477aa23876c63898a0a
Sam-Fishwick <sam.fishwick@hotmail.com>
2023-08-27 00:14:23 +0100

    Adding problem 125 to py easy

---
+++ Py/Easy/125_Valid_Palindrome.py
@@ -0,0 +1,23 @@
+#A phrase is a palindrome if, after converting all uppercase letters into 
+#lowercase letters and removing all non-alphanumeric characters, it reads the 
+#same forward and backward. Alphanumeric characters include letters and numbers.
+
+#Given a string s, return true if it is a palindrome, or false otherwise.
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) < 2:
+            return True
+        
+        s = ''.join(filter(str.isalnum, s))
+        s = s.lower()
+
+        i: int = 0
+        j: int = len(s)-1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i +=1
+            j -= 1
+        return True
